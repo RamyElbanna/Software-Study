2ae288ee3f2b22d3e1dd8776deba877dc96a8fa8
RamyAlbanna <ramyalbanna12@gmail.com>
2020-07-23 13:09:40 +0200

    linked list continious commit

---
+++ Data structures/LinedList/linkedList.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+class LinkedList{
+private:
+    struct Node{
+        int value;
+        Node* next;
+    };
+    Node* head;
+    int size;
+public:
+    LinkedList(){
+        size = 0;
+        head = nullptr;
+    }
+
+    bool IsEmpty(){
+        return size == 0 && head == nullptr;
+    }
+
+    int Size(){
+        return size;
+    }
+
+    void PushFront(int item){
+        Node* temp = new Node();
+        temp->value = item;
+        temp->next = head;
+        head = temp;
+        size++;
+    }
+
+    int PopFront(){
+        if(head == nullptr) return 0;
+        Node* temp = head;
+        int poppedValue = temp->value;
+        head = head->next;
+        delete(temp);
+        size--;
+        return poppedValue;
+    }
+
+    void Insert(int index, int item){
+        if(index == 0){
+            PushFront(item);
+            return;
+        }
+
+        if(index > size) throw("out of boundries");
+
+        Node* current = head;
+        for (int i = 0; i < index -1; i++)
+        {
+            current = current->next;
+        }
+
+        Node* newNode = new Node();
+        newNode->value = item;
+        newNode->next = current->next;
+        current->next = newNode;
+        size++;
+        
+    }
+
+    int ValueAt(int index){
+        if(size == 1) return head->value;
+        Node* current = head;
+        for (int i = 0; i < index; i++)
+        {
+            current = current->next;
+        }
+        return current->value;
+    }
+
+    void Display(){
+        cout << "the size of likedlist = " << size << endl;
+        Node* current = head;
+        while(current != nullptr){
+            cout << current->value << ",";
+            current = current->next;
+        }
+    }
+
+};
+
+int main(){
+    LinkedList ls = LinkedList();
+    ls.Insert(0,3);
+    ls.Display();
+    return 0;
+}
